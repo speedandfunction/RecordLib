41a062402cd2dd44c1599f9ff6cb78fb912a7c3a
Devra Garfinkle Johnson <devra.johnson@verizon.net>
2019-08-29 13:43:07 -0400

    Django rest (#56)
    * Fixed docker run command
    
    * Fixed docker run command
    
    * Add a Django REST backend
    
    This is the start of a Django REST backend.
    The backend is meant to be paired with a
    React frontend.  The Django app is in the
    directory "backend".   Currently, you can POST
    a Summary pdf file to localhost:8000/upload.
    The app will parse the file and return JSON with
    the defendant's information and a list of the cases
    from the file.
    
    * Add a React frontend
    
    This is the start of a React frontend.
    Currently, you can upload a Summary PDF.
    The app will display information from the `CRecord`
    generated from the Summary.
    
    For instructions, see the React App section of the Readme.
    
    * Edit React App instructions
    
    * Add instructions for app development
    
    * Add instructions and code fix for pdftotext
    
    The Readme now has instructions for obtaining pdftotext.
    Code has been edited to specify that the file produced by
    pdftotext should be read with the uft8 encoding
    (for operating systems for which this is not the default.)
    
    * Remove unneeded import of pytest
    
    pytest is a dev-package.
    
    * Add Font Awesome icons
    
    * Add and integrate a show/hide list component
    
    * Upgraded eslint-utils to resolve a vulnerability
    
    * Add comments to backend
    
    * Add PropTypes and comments
    
    In React, PropTypes do some of the work of commenting.
    
    This commit also includes some refactoring.
    Specifically, props are now destructured.
    I also fixed some bugs in the code for ids for the React lists.

--- frontend/src/components/Cases.js
+++ frontend/src/components/Cases.js
@@ -1,11 +1,15 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 import Case from "./Case";
 import ShowHideList from "./ShowHideList";
 
 
 function Cases(props) {
     const casesStyle = {margin: '15px', padding: '10px', width: '900px'};
-    const casesRendered = props.cases.map((caseObject, index) => {
+    const cases = props.cases
+    const casesRendered = cases.map((caseObject, index) => {
+
             const id = caseObject.docket_number;
             return <Case {...caseObject} key={id} id={id}/>
         }
@@ -17,5 +21,10 @@ function Cases(props) {
         </div>
     );
 }
+
+Cases.propTypes = {
+    cases: PropTypes.array.isRequired
+}
+
 
 export default Cases;
