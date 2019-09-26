ec7f0dc89ddaa72b1c3a218f6929871fb54f06f3
Nate Vogel <nathan.c.vogel@gmail.com>
2019-09-26 15:35:37 -0400

    fixed test of rendering petitions

--- tests/test_petitions.py
+++ tests/test_petitions.py
@@ -14,23 +14,22 @@ def test_file_name(example_attorney, example_person, example_case):
 # EXPUNGEMENT PETITIONS
 def test_expungement_petition(example_attorney, example_person, example_case):
     p = Expungement(attorney=example_attorney, client=example_person, cases=[example_case], 
-                    type=Expungement.FULL_EXPUNGEMENT,
-                    procedure=Expungement.NONSUMMARY_EXPUNGEMENT)
+                    expungement_type=Expungement.ExpungementTypes.FULL_EXPUNGEMENT,
+                    procedure=Expungement.ExpungementProcedures.NONSUMMARY_EXPUNGEMENT)
     assert p.cases[0] == example_case
-    assert p.type == Expungement.FULL_EXPUNGEMENT
-    assert p.procedure == Expungement.NONSUMMARY_EXPUNGEMENT
+    assert p.expungement_type == Expungement.ExpungementTypes.FULL_EXPUNGEMENT
+    assert p.procedure == Expungement.ExpungementProcedures.NONSUMMARY_EXPUNGEMENT
 
 def test_render_expungement_petition(example_person, example_attorney, example_case): 
     p = Expungement(attorney=example_attorney, client=example_person, cases=[example_case], 
-                    type=Expungement.FULL_EXPUNGEMENT)
+                    expungement_type=Expungement.ExpungementTypes.FULL_EXPUNGEMENT)
     with open("tests/templates/790ExpungementTemplate_usingpythonvars.docx", "rb") as doc:
         p.set_template(doc)
 
     doc = p.render()
     # breakpoint()
     # use doc.save() to manually inspect the rendered petition.
-    assert isinstance(doc, io.BytesIO)
-
+    assert "docx" in doc.__dict__.keys()
 
 # SEALING PETITIONS
 def test_sealing_petition(example_person, example_case):
