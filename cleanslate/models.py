977becbeca3812bdd201ee95ad334eb5ae937ce4
Nate Vogel <nathan.c.vogel@gmail.com>
2019-10-28 16:24:39 -0400

    store petition template in the db as bytes, and get it back out as a bytesio object that is passed to RecordLib.Petition objects with set_template

--- cleanslate/models.py
+++ cleanslate/models.py
@@ -1,7 +1,13 @@
 from django.db import models
-
+import io
 
 class PetitionTemplate(models.Model):
     name = models.CharField(max_length=255)
     doctype = models.CharField(max_length=255)  # perhaps should be a reference table of supported types.
     data = models.BinaryField()
+
+    def data_as_bytesio(self):
+        """
+        DB stores `data` as bytes, but we more often want to use a BytesIO object.
+        """
+        return io.BytesIO(self.data)
