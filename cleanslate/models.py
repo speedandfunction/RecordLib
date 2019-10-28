da7fd2ecc01103f44e27981b4e114f0d04f7c234
Nate Vogel <nathan.c.vogel@gmail.com>
2019-10-28 16:25:58 -0400

    added comment explaining petition template model a little

--- cleanslate/models.py
+++ cleanslate/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import io
 
 class PetitionTemplate(models.Model):
+    """Model for storing a docx template for expungement or sealing petitions."""
+    
     name = models.CharField(max_length=255)
     doctype = models.CharField(max_length=255)  # perhaps should be a reference table of supported types.
     data = models.BinaryField()
