da7a904a24208daae277a82afad2a23f748d0a9a
Nate Vogel <nathan.c.vogel@gmail.com>
2019-11-25 15:20:43 -0500

    ujs now can search the cp site for a person's name w/out needing to use a headless browser.

--- tests/ujs/test_services.py
+++ tests/ujs/test_services.py
@@ -1,10 +1,37 @@
 import pytest
 from ujs.models import SourceRecord
 from ujs.services import download
-from datetime import datetime
+from ujs.services.UJSSearch import UJSSearch 
+from datetime import datetime, date
 import logging
 import time
 import requests
+
+
+# TODO: mock this with the following,
+#  if I can figure out a way for that still to be a worthwhile test. 
+# [{'docket_number': 'CP-46-CR-0008423-2015', 'docket_sheet_url': 'CPReport.ashx?docketNumber=CP-46-CR-0008423-2015&dnh=ZvuxhBGDxBDVzE1TXOV00Q%3d%3d', 'summary_url': 'CourtSummaryReport.ashx?docketNumber=CP-46-CR-0008423-2015&dnh=ZvuxhBGDxBDVzE1TXOV00Q%3d%3d', 'caption': 'Comm. v. Kane, Kathleen Granahan', 'case_status': 'Closed', 'otn': 'T7090322', 'dob': '6/14/1966'}, {'docket_number': 'CP-46-MD-0002457-2015', 'docket_sheet_url': 'CPReport.ashx?docketNumber=CP-46-MD-0002457-2015&dnh=IKyQqgkSTZdotOIfTavQwQ%3d%3d', 'summary_url': 'CourtSummaryReport.ashx?docketNumber=CP-46-MD-0002457-2015&dnh=IKyQqgkSTZdotOIfTavQwQ%3d%3d', 'caption': 'Comm v  Kane, Kathleen Granahan', 'case_status': 'Closed', 'otn': 'T7090322', 'dob': '6/14/1966'}, {'docket_number': 'CP-46-CR-0006239-2015', 'docket_sheet_url': 'CPReport.ashx?docketNumber=CP-46-CR-0006239-2015&dnh=ljFLOabFyPEOG9nfpg%2bOTA%3d%3d', 'summary_url': 'CourtSummaryReport.ashx?docketNumber=CP-46-CR-0006239-2015&dnh=ljFLOabFyPEOG9nfpg%2bOTA%3d%3d', 'caption': 'Comm. v. Kane, Kathleen Granahan', 'case_status': 'Closed', 'otn': 'T6863802', 'dob': '6/14/1966'}]
+
+
+def test_cp_search():
+    cp_searcher = UJSSearch.use_court("CP")
+    results = cp_searcher.search_name(
+        last_name="Kane", first_name="Kathleen", 
+        dob=date(year=1966, month=6, day=14))
+    assert len(results) > 0 
+    try:
+        for r in results:
+            r["docket_number"]
+    except KeyError:
+        pytest.raises("Search Results missing docket number.")
+
+def test_cp_search_no_results():
+    cp_searcher = UJSSearch.use_court("CP")
+    results = cp_searcher.search_name(
+        first_name="Ferocity", last_name="Wimbledybear") 
+    assert len(results) == 0 
+ 
+
 
 class FakeResponse:
 
