059b92101fc2e913ac3df4c0c24993de11a7b552
Nate Vogel <nathan.c.vogel@gmail.com>
2020-01-03 13:18:32 -0500

    added a management command to the grades app, which provides for uploading a csv of charge-records (a charge and a grade and a weight for the given charge and grade) to a remote site.

--- grades/management/commands/load_charges.py
+++ grades/management/commands/load_charges.py
@@ -9,9 +9,9 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     """
-    Load a file of charges into the database.
+    Load a file of charges into the database of a locally running grades app.
     """
-    help = "Load charge records into the database from a csv file"
+    help = "Load charge records into the database of a local grades app from a csv file"
 
     def add_arguments(self, parser):
         parser.add_argument('filepath')
