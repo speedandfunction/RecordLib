3e0abffc987f3ec42ad8c626bcc53c0e81f04ad1
Nate Vogel <nathan.c.vogel@gmail.com>
2020-06-22 10:29:32 -0400

    fixed merge commit issues

--- scripts/download_dockets.py
+++ scripts/download_dockets.py
@@ -26,13 +26,8 @@ def download(url, dest_path, name, doc_type):
     return
 
 
-<<<<<<< HEAD
-def download_docket(scraper_url: str, court: str, docket_number: str, doc_type: str):
-    """ Download a single docket using the DocketScraperAPI.
-=======
 def download_docket(docket_number: str, doc_type: str):
     """ Download a single docket from the UJS portal..
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
 
     Args:
         docket_number: the docket number.
@@ -42,28 +37,6 @@ def download_docket(docket_number: str, doc_type: str):
         If successful, a tuple with the url of the downloaded file
         as well as the downloaded file. Otherwise, a tuple (None, None)
     """
-<<<<<<< HEAD
-    resp = requests.post(
-        f"{scraper_url}/ujs/search/docket/", json={"docket_number": docket_number}
-    )
-    if resp.json().get("status") and "Error" in resp.json().get("status"):
-        logging.error("Error from ScraperAPI:" + resp.json().get("status"))
-        return None, None
-    if (
-        resp.status_code == 200
-        and resp.json().get("searchResults") is not None
-        and len(resp.json().get("searchResults")) > 0
-    ):
-        logging.info("... URL found. Downloading file.")
-        if doc_type.lower() in ["s", "summary", "summaries"]:
-            # download the summary
-            try:
-                url = resp.json()["searchResults"][0]["summary_url"]
-            except:
-                url = None
-        else:
-            try:
-=======
     try:
 
         resp = searchujs.search_by_docket(docket_number)
@@ -77,11 +50,6 @@ def download_docket(docket_number: str, doc_type: str):
             url = resp[0]["summary_url"]
         else:
             url = resp[0]["docket_sheet_url"]
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
-
-                url = resp.json()["searchResults"][0]["docket_sheet_url"]
-            except:
-                url = None
         resp = requests.get(url, headers={"User-Agent": "DocketAnalyzerTesting"})
         if resp.status_code == 200:
             logging.info("Docket found and downloaded.")
@@ -91,10 +59,6 @@ def download_docket(docket_number: str, doc_type: str):
                 f"...request for url failed. Status code: { resp.status_code }"
             )
             logging.error(f"   URL was { url }")
-<<<<<<< HEAD
-        logging.info("... Downloading complete. Moving on.")
-=======
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
     else:
         logging.info("No docket found.")
 
@@ -103,10 +67,6 @@ def download_docket(docket_number: str, doc_type: str):
 
 @cli.command()
 @click.option("-p", "--dest-path", default="tests/data", show_default=True)
-<<<<<<< HEAD
-@click.option("-u", "--scraper-url", default="http://localhost:5000", show_default=True)
-=======
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
 @click.option(
     "-dt",
     "--doc-type",
@@ -124,16 +84,7 @@ def download_docket(docket_number: str, doc_type: str):
     type=click.Choice(["CP", "MDJ", "both"]),
 )
 def names(
-<<<<<<< HEAD
-    dest_path: str,
-    scraper_url: str,
-    doc_type: str,
-    input_csv: str,
-    output_csv: str,
-    court: str,
-=======
     dest_path: str, doc_type: str, input_csv: str, output_csv: str, court: str,
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
 ) -> None:
     """Download dockets from a list of names.
     TODO need to be able to search both CP and MDJ dockets in one call of this cli.
@@ -168,31 +119,6 @@ def names(
             last_name = name[-1]
             dob = datetime.strptime(row["DOB"], r"%m/%d/%Y")
             for court_to_search in courts:
-<<<<<<< HEAD
-                resp = requests.post(
-                    f"{scraper_url}/searchName/{court_to_search}",
-                    json={
-                        "first_name": first_name,
-                        "last_name": last_name,
-                        "dob": row["DOB"],
-                    },
-                )
-                if resp.status_code == 200:
-                    logging.info(f"Successful search for {row['Name']}")
-                    if doc_type.lower() in ["s", "summary", "summaries", "both"]:
-                        # download the summary
-                        try:
-                            logging.info("... Downloading summary")
-                            row["url"] = resp.json()["dockets"][0]["summary_url"]
-                            row["doctype"] = "summary"
-                            if output_csv:
-                                writer.writerow(row)
-                            download(row["url"], dest_path, row["Name"], doc_type)
-                        except:
-                            logging.error(f"... No summary found for {row['Name']}.")
-                            row["url"] = ""
-                            row["doctype"] = "none"
-=======
                 results = searchujs.search_by_name(
                     first_name, last_name, dob, court=court_to_search
                 )
@@ -214,7 +140,6 @@ def names(
                                 )
                                 row["url"] = ""
                                 row["doctype"] = "none"
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
                     if doc_type.lower() in ["d", "docket", "both"]:
                         logging.info("... Downloading dockets")
                         for i, result in enumerate(results[court_to_search]):
@@ -227,21 +152,12 @@ def names(
                                     row["url"],
                                     dest_path,
                                     row["Name"] + "_" + str(i),
-<<<<<<< HEAD
-                                    doc_type,
-                                )
-                        except:
-                            logging.error(f" No dockets found for {row['Name']}.")
-                            row["url"] = ""
-                            row["doctype"] = "none"
-=======
                                     "docket",
                                 )
                             except:
                                 logging.error(f" No dockets found for {row['Name']}.")
                                 row["url"] = ""
                                 row["doctype"] = "none"
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
                 else:
                     logging.warning(f"Did not find any results for {row['Name']}")
                 if output_csv:
@@ -264,7 +180,7 @@ def names(
 )
 @click.option("-o", "--output", required=True)
 def docket_number(docket_number: str, doc_type: str, output: str):
-    url, resp_content = download_docket(docket_number, doc_type)
+    _, resp_content = download_docket(docket_number, doc_type)
     if resp_content is not None:
         click.echo("Downloaded docket.")
         with open(output, "wb") as f:
@@ -274,10 +190,6 @@ def docket_number(docket_number: str, doc_type: str, output: str):
 
 @cli.command()
 @click.option("-p", "--dest-path", default="tests/data", show_default=True)
-<<<<<<< HEAD
-@click.option("-u", "--scraper-url", default="http://localhost:5000", show_default=True)
-=======
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
 @click.option(
     "-dt",
     "--doc-type",
@@ -287,13 +199,8 @@ def docket_number(docket_number: str, doc_type: str, output: str):
 )
 @click.option("-i", "--input-csv", required=True)
 @click.option("-o", "--output-csv", default=None)
-<<<<<<< HEAD
-def docket_numbers(
-    dest_path: str, scraper_url: str, doc_type: str, input_csv: str, output_csv: str
-=======
 def docket_number_file(
     dest_path: str, doc_type: str, input_csv: str, output_csv: str
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
 ) -> None:
     """
     Download dockets or summary sheets for the docket numbers listed in <input-csv>
@@ -314,14 +221,7 @@ def docket_number_file(
             return
         for row in reader:
             docket_number = row["Docket Number"]
-            court = "CP" if "CP-" in docket_number else "MDJ"
-<<<<<<< HEAD
-            url, resp_content = download_docket(
-                scraper_url, court, docket_number, doc_type
-            )
-=======
             url, resp_content = download_docket(docket_number, doc_type)
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
             if resp_content is not None:
                 with open(
                     os.path.join(dest_path, f"{ docket_number }_{ doc_type }.pdf"),
@@ -362,14 +262,7 @@ def random(document_type: str, number: int, dest_path: str, court: str) -> None:
             logging.info("court is now " + court_to_search)
         else:
             court_to_search = court
-<<<<<<< HEAD
-
-        _, resp_content = download_docket(
-            scraper_url, court_to_search, docket_number, document_type
-        )
-=======
-        url_to_fetch, resp_content = download_docket(docket_number, document_type)
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
+        _, resp_content = download_docket(docket_number, document_type)
         if resp_content is not None:
             with open(
                 os.path.join(dest_path, f"{ docket_number }_{ document_type }.pdf"),
@@ -410,7 +303,3 @@ def urls(url_file: str, dest_dir: str, nest_dirs: bool):
             else:
                 file_dest = dest_dir
             download(row["url"], file_dest, person_name, row["type"])
-<<<<<<< HEAD
-=======
-
->>>>>>> 4915274440201985511180069552af9aa7c5f4da
