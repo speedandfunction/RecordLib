cdde3c615a45d1ac92f6bcf72659ed7bc223662b
Nate Vogel <nathan.c.vogel@gmail.com>
2020-07-14 16:32:07 -0400

    added a test for adding a new case to a petition

--- tests/frontend/src/reducers/petitions.test.js
+++ tests/frontend/src/reducers/petitions.test.js
@@ -2,7 +2,11 @@ const { TestScheduler } = require("jest");
 import petitionsReducer, {
   petitionCollectionReducer,
 } from "frontend/src/reducers/petitions";
-import { newPetition, updatePetition } from "frontend/src/actions/petitions";
+import {
+  newPetition,
+  updatePetition,
+  newPetitionCase,
+} from "frontend/src/actions/petitions";
 
 describe("slice of state for the petitionCollection", () => {
   test("initial state", () => {
@@ -110,7 +114,125 @@ describe("slice of state for the petitionCollection", () => {
     });
   });
 
-  test("update cases on a petition", () => {
+  test("add new case to a petition", () => {
+    /**
+     * we need to be able to add a new case to a petition.
+     * this reducer will need to take a petitionID and the id of a new case to tie to that petition.
+     *
+     * The reducer will need to append that new case id to the list of cases in the given petition.
+     * And it will need to to create at least a key w/ empty object in the entities.cases collection.
+     */
+    const startingState = {
+      petitionUpdates: { updateInProgress: false },
+      petitionCollection: {
+        editingPetitionId: "0",
+        entities: {
+          petitions: {
+            "0": {
+              id: "0",
+              attorney: {
+                organization: "Legal Aid Org",
+                full_name: "Abraham Lincoln",
+                organization_address: {
+                  line_one: "1234 S. St.",
+                  city_state_zip: "Phila PA",
+                },
+                organization_phone: "123-123-1234",
+                bar_id: "11222",
+              },
+              client: { first_name: "Suzy", last_name: "Smith" },
+              cases: ["12-CP-12-CR-1234567"],
+              expungement_type: "FULL_EXPUNGEMENT",
+              petition_type: "Expungment", // as opposed to "Sealing",
+              summary_expuntement_language:
+                "and Petitioner is over 70 years old and has been free of arrest for more than ten years since this summary conviction.",
+              service_agencies: ["The Zoo", "Jims Pizza Palace"],
+              include_crim_hist_report: "",
+              ifp_message: "Please allow this petition.",
+            },
+          },
+          cases: {
+            "12-CP-12-CR-1234567": {
+              id: "12-CP-12-CR-1234567",
+              docket_number: "12-CP-12-CR-1234567",
+              affiant: "John",
+              status: "closed",
+              county: "Montgomery",
+              charges: ["12-CP-12-CR-1234567charges@0"],
+              editing: false,
+            },
+          },
+          charges: {
+            "12-CP-12-CR-1234567charges@0": {
+              id: "12-CP-12-CR-1234567charges@0",
+              statute: "endangering othrs.",
+            },
+          },
+        },
+        petitionIds: ["0"],
+      },
+    };
+
+    const updateAction = newPetitionCase("0", "20-CP-20-CR-1234567");
+    const modifiedState = petitionsReducer(startingState, updateAction);
+    expect(modifiedState).toEqual({
+      petitionUpdates: { updateInProgress: false },
+      petitionCollection: {
+        editingPetitionId: "0",
+        entities: {
+          petitions: {
+            "0": {
+              id: "0",
+              attorney: {
+                organization: "Legal Aid Org",
+                full_name: "Abraham Lincoln",
+                organization_address: {
+                  line_one: "1234 S. St.",
+                  city_state_zip: "Phila PA",
+                },
+                organization_phone: "123-123-1234",
+                bar_id: "11222",
+              },
+              client: { first_name: "Suzy", last_name: "Smith" },
+              cases: ["12-CP-12-CR-1234567", "20-CP-20-CR-1234567"],
+              expungement_type: "FULL_EXPUNGEMENT",
+              petition_type: "Expungment", // as opposed to "Sealing",
+              summary_expuntement_language:
+                "and Petitioner is over 70 years old and has been free of arrest for more than ten years since this summary conviction.",
+              service_agencies: ["The Zoo", "Jims Pizza Palace"],
+              include_crim_hist_report: "",
+              ifp_message: "Please allow this petition.",
+            },
+          },
+          cases: {
+            "12-CP-12-CR-1234567": {
+              id: "12-CP-12-CR-1234567",
+              docket_number: "12-CP-12-CR-1234567",
+              affiant: "John",
+              status: "closed",
+              county: "Montgomery",
+              charges: ["12-CP-12-CR-1234567charges@0"],
+              editing: false,
+            },
+            "20-CP-20-CR-1234567": {
+              id: "20-CP-20-CR-1234567",
+              docket_number: "20-CP-20-CR-1234567",
+              editing: false,
+            },
+          },
+          charges: {
+            "12-CP-12-CR-1234567charges@0": {
+              id: "12-CP-12-CR-1234567charges@0",
+              statute: "endangering othrs.",
+            },
+          },
+        },
+        petitionIds: ["0"],
+      },
+    });
+  });
+
+  test("update a petition", () => {
     const startingState = {
       petitionUpdates: { updateInProgress: false },
       petitionCollection: {
@@ -166,7 +288,6 @@ describe("slice of state for the petitionCollection", () => {
 
     const updateAction = updatePetition("0", {
       attorney: { organization: "New Org" },
-      cases: { "12-CP-12-CR-1234567": { affiant: "Jill" } },
     });
     const newState = petitionsReducer(startingState, updateAction);
     expect(newState).toEqual({
@@ -202,7 +323,7 @@ describe("slice of state for the petitionCollection", () => {
             "12-CP-12-CR-1234567": {
               id: "12-CP-12-CR-1234567",
               docket_number: "12-CP-12-CR-1234567",
-              affiant: "Jill",
+              affiant: "John",
               status: "closed",
               county: "Montgomery",
               charges: ["12-CP-12-CR-1234567charges@0"],
